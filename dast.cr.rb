8d4cb9742340b726dbed2d320c172e1564842786
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-22 14:39:14 +0900

    fix

--- dast.cr.rb
+++ dast.cr.rb
@@ -2,7 +2,7 @@ class DastCr < Formula
   desc "CLI tool to display the date list."
   homepage "https://github.com/at-grandpa/dast.cr"
   url "https://github.com/at-grandpa/dast.cr/releases/download/v0.1.0/dast-0.1.0-darwin-x86_64.tar.gz"
-  sha256 "6278fe039cf39471ff905ca51cdaf1bce1f13a3db0101870fd07cfbd013bdb46"
+  sha256 "2255bcff12b9258d12ba0d448a660273f80c7945ff23a7f9dd55971b8dd46994"
 
   def install
     bin.install "dast"
